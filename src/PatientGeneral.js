7e98553f6dab17fe71c54a3a6643286ff835f3ba
alista24ftp <johnlittlefish@hotmail.com>
2017-02-05 20:49:03 -0800

    modified styling for PatientGeneral, as well as frontend API call to patientuuid upon login

--- src/PatientGeneral.js
+++ src/PatientGeneral.js
@@ -12,26 +12,18 @@ class PatientGeneral extends Component {
           <p><b>Gender:</b> {this.props.generalInfo.gender}</p>
           <p><b>Date of Birth:</b> {this.props.generalInfo.dateOfBirth}</p>
           <p><b>UUID:</b> {this.props.generalInfo.patientUUID}</p>
-          <p><b>Age:</b> {this.props.generalInfo.age}</p>
+          <p><b>Blood Type:</b> {this.props.generalInfo.bloodType}</p>
           <p><b>Medical Number:</b> {this.props.generalInfo.medicalNumber}</p>
         </div>
         <div className="ContactInfo">
           <h4 className="moduleSubHeader">Patient Contacts</h4>
-          <p><b>Phone Number:</b> {this.props.generalInfo.phone}</p>
+          <p><b>Phone Number:</b> {this.props.generalInfo.phoneNumber}</p>
           <p><b>Emergency Contact:</b> {this.props.generalInfo.emergencyContact}</p>
           <p><b>Address:</b> {this.props.generalInfo.address}</p>
         </div>
-        <div className="Demographics">
-          <h4 className="moduleSubHeader">Demographics</h4>
-          <p><b>Ethnicity:</b> {this.props.generalInfo.ethnicity}</p>
-        </div>
         <div className="Notes">
           <h4 className="moduleSubHeader">Notes</h4>
-          <form>
-            <textarea>
-              {this.props.generalInfo.notes}
-            </textarea>
-          </form>
+          {this.props.generalInfo.notes}
         </div>
       </div>
     );
--- src/requests.js
+++ src/requests.js
@@ -29,7 +29,7 @@ var patientSearch = function(id){
 		  .get(goServer+'/patients/patientuuid/2779123f-f4c3-43ec-b20a-a6fcbb0eb418')
 		  .end(function(err, res){
 		    //if(!err && res.ok){
-				if(res.statusCode !== 400 || res.statusCode !== 404){
+				if(res.code !== 400 || res.code !== 404){
 					resolve(res.body);
 				}else {
 					reject();
